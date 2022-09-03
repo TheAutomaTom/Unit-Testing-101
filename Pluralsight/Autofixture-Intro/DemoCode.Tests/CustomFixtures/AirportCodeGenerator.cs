236636b34dd08da193a6e316e023aef5e2774174
Tom Grossi <TheAutomaTom@gmail.com>
2022-09-03 18:12:57 -0500

    Add the best part... custom specimen builders

---
+++ Pluralsight/Autofixture-Intro/DemoCode.Tests/CustomFixtures/AirportCodeGenerator.cs
@@ -0,0 +1,51 @@
+﻿using AutoFixture.Kernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoCode.Tests.CustomFixtures
+{
+	public class AirportCodeGenerator : ISpecimenBuilder
+	{
+		public object Create(object request, ISpecimenContext context)
+		{
+			// See if we're trying to create a value for a property
+			var propertyInfo = request as PropertyInfo;
+
+			if( propertyInfo is null )
+			{
+				// This specimen builder does not apply to current request
+				return new NoSpecimen();
+			}
+
+			// Now we have confirmed we are creating a property.
+			// Is it one we need custom logic for?
+
+			// This applies to DepartureAirportCode, ArrivalAirportCode, and AirportCode
+			var isAirportCode = propertyInfo.Name.Contains("AirportCode");
+
+			// and the property type is string... just to be safe.
+			var isStringProperty = propertyInfo.PropertyType == typeof(string);
+
+			if (!isAirportCode || !isStringProperty)
+			{
+				return new NoSpecimen();
+			}				
+			return RandomAirportCode();
+
+		}
+
+		private string RandomAirportCode()
+		{
+			if(DateTime.Now.Ticks % 2 == 0)
+			{
+				return "RDU";
+			}
+			return "VPS";
+		}
+
+	}
+}
---
+++ Pluralsight/Autofixture-Intro/DemoCode.Tests/CustomFixtures/CustomSpecimenDemos.cs
@@ -0,0 +1,45 @@
+﻿using AutoFixture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoCode.Tests.CustomFixtures
+{
+	public class CustomSpecimenDemos
+	{
+		[Fact]
+		public void BuiltInSpecimenBuilders() {
+
+			var fixture = new Fixture();
+
+			//This is using a built-in ISpecimenBuilder...
+			fixture.Customize(new CurrentDateTimeCustomization());
+			var date1 = fixture.Create<DateTime>(); //This will be "Now"
+
+			// This is another way to add the same generator...
+			fixture.Customizations.Add(new CurrentDateTimeGenerator());
+
+			// Assert...not implemented
+		}
+
+
+		[Fact]
+		public void CustomSpecimenBuilders()
+		{
+			var fixture = new Fixture();
+			fixture.Customizations.Add(new AirportCodeGenerator());
+
+			var flight = fixture.Create<FlightDetails>();
+
+			Assert.NotNull(flight.ArrivalAirportCode);
+			Assert.NotNull(flight.DepartureAirportCode);
+			Assert.NotNull(flight.AirlineName);
+
+			var airport = fixture.Create<Airport>();
+			Assert.NotNull(airport.AirportCode);
+		}
+
+	}
+}
